5f148693041ef27980196fea5fef3f9790b66719
Brandon Ramirez <brandon.ramirez@pernixlabs.com>
2025-04-13 22:45:12 -0600

    [Chapter 5] Added Device Information in About screen

--- app/(tabs)/about.tsx
+++ app/(tabs)/about.tsx
@@ -1,9 +1,22 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, Platform, StyleSheet } from "react-native";
 
 export default function About() {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>About Screen</Text>
+
+      <Text style={styles.text}>
+        {Platform.OS} {Platform.Version}
+      </Text>
+
+      {Platform.OS === "android" && (
+        <View>
+          <Text style={styles.text}>Android specific content</Text>
+          <Text style={styles.text}>
+            {JSON.stringify(Platform.constants, null, 2)}
+          </Text>
+        </View>
+      )}
     </View>
   );
 }
