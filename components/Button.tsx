ea7dee7a95c0205b9b728ca91287e0be9b7200a8
Brandon Ramirez <brandon.ramirez@pernixlabs.com>
2025-04-13 18:21:30 -0600

    [Chapter 3] Adding theme to button

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,10 +1,36 @@
 import { Pressable, StyleSheet, Text, View } from "react-native";
+import { FontAwesome } from "@expo/vector-icons";
 
 interface Props {
   label: string;
+  theme?: "primary";
 }
 
-export const Button = ({ label }: Props) => {
+export const Button = ({ label, theme }: Props) => {
+  if (theme === "primary") {
+    return (
+      <View
+        style={[
+          styles.buttonContainer,
+          { borderWidth: 4, borderColor: "#ffd33d", borderRadius: 18 },
+        ]}
+      >
+        <Pressable
+          style={[styles.button, { backgroundColor: "#fff" }]}
+          onPress={() => alert("You pressed a button")}
+        >
+          <FontAwesome
+            name="picture-o"
+            size={18}
+            color="#25292e"
+            style={styles.buttonIcon}
+          />
+          <Text style={[styles.buttonLabel, { color: "#2529e" }]}>{label}</Text>
+        </Pressable>
+      </View>
+    );
+  }
+
   return (
     <View style={styles.buttonContainer}>
       <Pressable style={styles.button} onPress={() => alert("Button Pressed!")}>
@@ -30,6 +56,9 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
     flexDirection: "row",
+  },
+  buttonIcon: {
+    paddingRight: 8,
   },
   buttonLabel: {
     color: "#fff",
